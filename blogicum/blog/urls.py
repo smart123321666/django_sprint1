d870fa3f51ac24cd9accc454b027f61cede9280c
Даниил Соколов <scake14@yandex.ru>
2023-08-07 23:34:25 +0500

    fourth try

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
     path('posts/<int:id>/', views.post_detail, name='post_detail'),
     path('category/<slug:category_slug>/', views.category_posts,
          name='category_posts'),
--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -2,12 +2,10 @@ from django.shortcuts import render
 
 
 def about(request):
-    # Логика обработки запроса
     template = 'pages/about.html'
     return render(request, template)
 
 
 def rules(request):
-    # Логика обработки запроса
     template = 'pages/rules.html'
     return render(request, template)
